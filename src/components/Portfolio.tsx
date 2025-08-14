447ab93129624302ece0bf193bd97384f1e22cbd
BKDR01 <b0327866@gmail.com>
2025-08-14 16:06:14 +0500

    update portfolio

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -12,7 +12,7 @@ const Portfolio: React.FC = () => {
       descriptionKey: 'portfolio.projects.ecommerce.description',
       technologies: ['React', 'tailwind'],
       image: 'https://media.istockphoto.com/id/1467812717/ru/%D1%84%D0%BE%D1%82%D0%BE/%D1%82%D1%80%D0%B0%D0%BA%D1%82%D0%BE%D1%80-%D1%81-%D0%BF%D1%80%D0%B8%D1%86%D0%B5%D0%BF%D0%BE%D0%BC-%D0%BD%D0%B0-%D1%81%D1%82%D0%B5%D1%80%D0%BD%D0%B5%D0%B2%D0%BE%D0%BC-%D0%BF%D0%BE%D0%BB%D0%B5-%D0%B2-%D0%BE%D0%B6%D0%B8%D0%B4%D0%B0%D0%BD%D0%B8%D0%B8-%D0%BF%D0%BE%D0%B3%D1%80%D1%83%D0%B7%D0%BA%D0%B8-%D0%B7%D0%B5%D1%80%D0%BD%D0%B0.jpg?s=612x612&w=0&k=20&c=7sAISNFRIJVua_JyCg6IdYZIiC_KjpdhWubl6aEQAGY=',
-      href: '#'
+      href: 'https://bkdr01.github.io/Track/'
     },
     {
       id: 2,
